69e3e6e40d38a0a67315558fdd9e6b2ac77b4ffb
Mark Jenkins <mark1@umd.edu>
2017-02-19 15:12:53 -0800

    Completed PIDController tests for basic acceleration and deceleration with Kp only non-zero, Ki only non-zero, and Kd only non-zero

--- test/PIDControllerTest.cpp
+++ test/PIDControllerTest.cpp
@@ -14,12 +14,63 @@
 #include <memory>
 #include "../app/PIDController.hpp"
 
-TEST(PIDControllerTest, testProportionalCoefficient) {
-  std::shared_ptr<PIDController> aController =
+// Test acceleration and deceleration with only Kp non-zero
+TEST(PIDControllerTest, testKpBasicAcelDecel) {
+  std::shared_ptr<PIDController> theController =
       std::make_shared<PIDController>();
- aController->reset();
- aController->setKp(5);
-  EXPECT_EQ(0, aController->computeCorrection(0, 0, 1));
-//  EXPECT_EQ(25, aController->computeCorrection(5, 0, 2));
+
+  // Establish basic testing parameters
+  theController->reset();
+  theController->setKp(5);
+  theController->setKi(0);
+  theController->setKd(0);
+
+  // Progress through modeled behavior
+  EXPECT_DOUBLE_EQ(0, theController->computeCorrection(0, 0, 1));
+  EXPECT_DOUBLE_EQ(25, theController->computeCorrection(5, 0, 2));
+  EXPECT_DOUBLE_EQ(0, theController->computeCorrection(5, 5, 3));
+  EXPECT_DOUBLE_EQ(-25, theController->computeCorrection(0, 5, 4));
+  EXPECT_DOUBLE_EQ(0, theController->computeCorrection(0, 0, 5));
 }
 
+// Test acceleration and deceleration with only Ki non-zero
+TEST(PIDControllerTest, testKiBasicAcelDecel) {
+  std::shared_ptr<PIDController> theController =
+      std::make_shared<PIDController>();
+
+  // Establish basic testing parameters
+  theController->reset();
+  theController->setKp(0);
+  theController->setKi(5);
+  theController->setKd(0);
+
+  // Progress through modeled behavior
+  EXPECT_DOUBLE_EQ(0, theController->computeCorrection(0, 0, 1));
+  EXPECT_DOUBLE_EQ(25, theController->computeCorrection(5, 0, 2));
+  EXPECT_DOUBLE_EQ(25, theController->computeCorrection(5, 5, 3));
+  EXPECT_DOUBLE_EQ(0, theController->computeCorrection(0, 5, 4));
+  EXPECT_DOUBLE_EQ(-25, theController->computeCorrection(0, 5, 5));
+  EXPECT_DOUBLE_EQ(0, theController->computeCorrection(0, -5, 6));
+  EXPECT_DOUBLE_EQ(0, theController->computeCorrection(0, 0, 7));
+}
+
+// Test acceleration and deceleration with only Kd non-zero
+TEST(PIDControllerTest, testKdBasicAcelDecel) {
+  std::shared_ptr<PIDController> theController =
+      std::make_shared<PIDController>();
+
+  // Establish basic testing parameters
+  theController->reset();
+  theController->setKp(0);
+  theController->setKi(0);
+  theController->setKd(5);
+
+  // Progress through modeled behavior
+  EXPECT_DOUBLE_EQ(0, theController->computeCorrection(0, 0, 1));
+  EXPECT_DOUBLE_EQ(25, theController->computeCorrection(5, 0, 2));
+  EXPECT_DOUBLE_EQ(-25, theController->computeCorrection(5, 5, 3));
+  EXPECT_DOUBLE_EQ(0, theController->computeCorrection(5, 5, 4));
+  EXPECT_DOUBLE_EQ(-25, theController->computeCorrection(0, 5, 5));
+  EXPECT_DOUBLE_EQ(25, theController->computeCorrection(0, 0, 6));
+  EXPECT_DOUBLE_EQ(0, theController->computeCorrection(0, 0, 7));
+}
